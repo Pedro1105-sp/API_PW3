5f313921ed7c6ceda11274baf05a4387d074a5b0
Pedro1105-sp <pedro.henriquerealalvessp@gmail.com>
2022-09-28 20:24:56 -0300

    Criação do arquivo FabricanteModel
    Criação da Tabela Fabricante

--- model/FabricanteModel.js
+++ model/FabricanteModel.js
@@ -1,1 +1,20 @@
+const { type } = require("express/lib/response");
 const Sequelize = require("sequelize");
+
+const connection = require("../database/database");
+
+const Fabricante = connection.define(
+    "TBL_FABRICANTE",
+    {
+        nome_fabricante:{
+            type: Sequelize.STRING(100),
+            allowNull: false
+        },
+        cnpj_fabricante:{
+            type: Sequelize.INTEGER(14),
+            allowNull: false
+        }
+    }
+);
+
+module.exports = Fabricante;
