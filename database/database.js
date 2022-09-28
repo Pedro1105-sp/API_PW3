88bad1515403a84e42e6cd4091aa16b19ed45451
Pedro1105-sp <pedro.henriquerealalvessp@gmail.com>
2022-09-28 20:13:06 -0300

    Criação do arquivo database
    Configuração do banco de dados.

--- database/database.js
+++ database/database.js
@@ -1,5 +1,12 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize(
-    "API_CARS"
+    "API_CARS",
+    "root",
+    "",
+    {
+        host: "localhost",
+        dialect: "mysql"
+    }
 );
+module.exports = connection;
